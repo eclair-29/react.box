8d36c7611a58aed0036a046e31b5d0b18ca7c6bf
Migz De Chavez <dmgzky@gmail.com>
2018-10-07 13:46:09 +0800

    marioplan: l19-l20

--- marioplan/src/components/projects/ProjectDetails.js
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -1,22 +1,40 @@
 import React from  'react' 
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
-const ProjectDetails = props => {
-    const id = props.match.params.id
-
-    return (
-        <div className="project-details wrapper">
-            <div className="header">
-                <h4 className="title">Project Title - { id }</h4>
-                <span>Posted by: Miguel</span>
+const ProjectDetails = ({ project }) => {
+    if (project) {
+        return (
+            <div className="project-details wrapper">
+                <div className="header">
+                    <h4 className="title">{ project.title }</h4>
+                    <span>Posted by: { project.author_fname } { project.author_lname }</span>
+                </div>
+                <div className="content">{ project.content }</div>
+                <div className="footer">
+                    <span>29th November, 2am</span>
+                </div>
             </div>
-            <div className="content">
-               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic temporibus, earum non veritatis voluptate sed officia harum debitis aperiam nam.
-            </div>
-            <div className="footer">
-                <span>29th November, 2am</span>
-            </div>
-        </div>
-    )   
+        )
+    } else {
+        return <p style={{ textAlign: 'center', marginTop: '40px' }}>Loading Project...</p>
+    }
 }
 
-export default ProjectDetails
+const mapStateToProps = (state, own_props) => {
+    let id = own_props.match.params.id
+    let projects = state.firestore.data.projects 
+    let project = projects ? projects[id] : null
+
+    return {
+        project: project
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(ProjectDetails)
--- marioplan/src/store/reducers/root_reducer.js
+++ marioplan/src/store/reducers/root_reducer.js
@@ -4,9 +4,13 @@ import project_reducer from './project_reducer'
 /* redux combine reducers */
 import { combineReducers } from 'redux'
 
+/* firestore reducer */
+import { firestoreReducer } from 'redux-firestore'
+
 const root_reducer = combineReducers({
     auth: auth_reducer,
-    project: project_reducer 
+    project: project_reducer ,
+    firestore: firestoreReducer
 })
 
 export default root_reducer
