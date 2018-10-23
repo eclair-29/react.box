c18bc3d62b3457ebe9cdbcb8abf514bf516e5cf2
Migz De Chavez <dmgzky@gmail.com>
2018-10-23 14:38:04 +0800

    marioplan: l35-l36

--- marioplan/functions/index.js
+++ marioplan/functions/index.js
@@ -3,6 +3,33 @@ const functions = require('firebase-functions');
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
+
+// // Firebase Admin
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+// // Create a notification collection to firestore
+const createNotification = notification => {
+    return admin.firestore()
+        .collection('notifications')
+        .add(notification)
+        .then(doc => console.log('Notification added: ', doc))
+}
+
+// // New Project Creation Function Trigger
+exports.projectCreated = functions.firestore
+    .document('projects/{projectId}')
+    .onCreate(doc => {
+        const project = doc.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.author_fname} ${project.author_lname}`,
+            timestamp: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification)
+    })
